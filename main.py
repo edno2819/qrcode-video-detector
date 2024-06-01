fc80cd3393c36bc2c9afe464ea78df36889cc8b8
edno <edno2819@hotmail.com>
2024-06-01 19:38:40 -0300

    ajust: code refactor and error handler

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import cv2
 from pyzbar.pyzbar import decode, ZBarSymbol
-import argparse
-def write_to_csv(data, output_file):
-    with open(output_file, 'a') as file:
-        file.write(data + '\n')
 
-def detect_qr_codes(image_path):
-    data = decode((image_path), symbols=[ZBarSymbol.QRCODE])
-    return data
+
+def write_to_csv(data, output_file):
+    try:
+        with open(output_file, 'a', encoding='utf-8') as file:
+            file.write(data + '\n')
+    except Exception as e:
+        print(f"Failed to write to CSV: {e}")
+
+
+def detect_qr_codes(image):
+    try:
+        data = decode(image, symbols=[ZBarSymbol.QRCODE])
+        return data
+    except Exception as e:
+        print(f"Failed to decode QR codes: {e}")
+        return []
+
 
 def extract_frames(video_path, output_file):
-    vidObj = cv2.VideoCapture(video_path)
-    count = 0
-    frame_counter = 0
-    success = 1
-    while success:
-        success, image = vidObj.read()
-        if success:
-            frame_counter += 1
-            if frame_counter % 60 == 0:
-                valuesDecoded = detect_qr_codes(image)
-                if len(valuesDecoded) != 0:
-                    write_to_csv(valuesDecoded[0].data.decode('utf-8'), output_file)
-            count += 1
-    vidObj.release()
+    try:
+        vidObj = cv2.VideoCapture(video_path)
+        frame_counter = 0
+        success = True
+        while success:
+            success, image = vidObj.read()
+            if success:
+                frame_counter += 1
+                if frame_counter % 60 == 0:
+                    valuesDecoded = detect_qr_codes(image)
+                    if len(valuesDecoded) != 0:
+                        write_to_csv(valuesDecoded[0].data.decode(
+                            'utf-8'), output_file)
+            else:
+                print("Erro finding or reading the video!")
+
+        vidObj.release()
+    except Exception as e:
+        print(f"Error processing video: {e}")
+
 
 if __name__ == "__main__":
+    import argparse
     parser = argparse.ArgumentParser(description='QR Code Reader')
     parser.add_argument('video_path', type=str, help='Path to the input video file')
     parser.add_argument('output_file', type=str, help='Path to the output CSV file')
